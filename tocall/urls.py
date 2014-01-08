abc1c5606ef269c7a349b46aceb8a88e8a1c6dbe
Kurt Ostergaard <yodude@gmail.com>
2014-01-08 15:05:19 -0500

    updated a list to generic view

--- tocall/urls.py
+++ tocall/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 
 from tocall import views
+from tocall.views import ContactListView
 
 urlpatterns = patterns('',
 	# ex: /tocall/
-	url(r'^$', views.index, name='index'),
+	url(r'^$', ContactListView.as_view(), name='contact-list'),
 	url(r'^list/$', views.list, name='list'),
 	url(r'^detail/(?P<id>\d+)/', views.detail, name='detail'),
 	url(r'^address_book/$', views.address_book, name='address_book'),
--- tocall/views.py
+++ tocall/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.views import generic
 
 from tocall.models import Contact, History
 
-def index(request):
-	return HttpResponse("this is the front page demo.")
+class ContactListView(generic.ListView):
+	
+	model = Contact
+
+	def get_context_data(self, **kwargs):
+		context = super(ContactListView, self).get_context_data(**kwargs)
+		return context
+
+class contact_detail_view(generic.DetailView):
+	model = Contact
 
 def list(request):
 	current_list = Contact.objects.order_by('next_call')
@@ -17,10 +28,12 @@ def detail(request, id):
 	context = {'contact': contact}
 	return render(request, 'tocall/detail.html', context)
 
+@login_required
 def address_book(request):
 	# addressbook = get_list_or_404(Contact)
 	# addressbook.order_by('last_name')
 	addressbook = Contact.objects.order_by('last_name')
+	# addressbook = Contact.objects.filter(user=User.username)
 	context = {'addressbook': addressbook}
 	return render(request, 'tocall/addressbook.html', context)
 
