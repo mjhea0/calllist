76219440c6c2af8c650f3bc05e2964ab019a99da
Kurt Ostergaard <yodude@gmail.com>
2014-01-09 16:56:23 -0500

    added full_name property to Contact model and templates

--- tocall/models.py
+++ tocall/models.py
@@ -17,6 +17,11 @@ class Contact(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def _get_full_name(self):
+        "returns the person's full name."
+        return '%s %s' % (self.first_name, self.last_name)
+    full_name = property(_get_full_name)
+
     def __unicode__(self):
         return self.last_name 
 
